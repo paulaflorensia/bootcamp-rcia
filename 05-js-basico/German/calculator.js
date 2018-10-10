ce96aedfa899253cb4c089167821c1d1ec86cd89
German969 <german.monzon.95@gmail.com>
2018-10-10 17:01:50 -0300

    Only one fnuction, adding counter function

--- 05-js-basico/German/calculator.js
+++ 05-js-basico/German/calculator.js
@@ -1,64 +1,42 @@
-/*-----Function for two values-----*/
-var globalResult;
-function calculator(val1, val2, operator){
-	var result;
-	var wrongOpTypes = ['number','boolean','undefined'];
-	var wrongValTypes = ['boolean','undefined'];
+function counterCalculator{
+	var globalResult = 0;
+	function calculator(val1, val2, operator){
+		var result;
+	
+		var validOperators = ['+','-','*','/'];
 
-	if((typeof val1 == 'string' || typeof val2 == 'string') && operator == '+'){
-		val1 = parseInt(val1);
-		val2 = parseInt(val2);
-	}
+		if(validOperators.includes(val2)){
+			operator = val2;
+			val2 = globalResult;
+		}
 
-	if(wrongOpTypes.includes(typeof operator)){
-		return "Wrong operator type";
-	}
-
-	if(wrongValTypes.includes(typeof val1) || wrongValTypes.includes(typeof val2)){
-		return "Wrong value type";
-	}
-
-	switch(operator) {
-    case '+':
-        result = val1+val2;
+		if(operator == '+' && (typeof val1 == 'string' || typeof val2 == 'string')){
+			val1 = parseInt(val1);
+			val2 = parseInt(val2);
+		}
+	
+		switch(operator) {
+ 		case '+':
+ 	       result = val1+val2;
+ 	       break;
+  		case '-':
+ 	       result = val1-val2;
         break;
-    case '-':
-        result = val1-val2;
-        break;
-    case '*':
-        result = val1*val2;
-        break;
-    case '/':
-        result = val1/val2;
-        break;
-    default:
-    	return "Invalid operator";
+ 		case '*':
+        	result = val1*val2;
+   	    	break;
+ 		case '/':
+ 	   		if(val2 == 0){
+ 	   			return "Can't divide by zero";
+ 	   			break;
+ 	   		}
+ 	       result = val1/val2;
+ 	       break;
+ 		default:
+ 	   	return "Invalid operator";
+		};
+	
+		this.globalResult = result;
+		return this.globalResult;
 	};
-
-	this.globalResult = result;
-	return this.globalResult;
-};
-/*calculator(6,5,'*');*/
-
-function calculator(val1, operator){
-	var result;
-	switch(operator) {
-    case '+':
-        result = val1+this.globalResult;
-        break;
-    case '-':
-        result = this.globalResult-val1;
-        break;
-    case '*':
-        result = val1*this.globalResult;
-        break;
-    case '/':
-        result = (this.globalResult) / val1;
-        break;
-    default:
-    	return "Invalid operator";
-	};
-	return result;
-};
-/*calculator(3,'/');*/
-
+}
